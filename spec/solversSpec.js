dd4db030493219825c27b11effbdeb77f20e8d79
Ying Hao Chan <yinghaochan@Yings-MacBook-Pro.local>
2015-10-13 17:08:51 -0700

    Reset spec to default

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -37,7 +37,7 @@ describe('solvers', function() {
 
     it('finds a valid solution for n of 0-7', function() {
       // Skip 2 and 3 because they have no solution.
-      [0, 1, 4, 5, 6, 7, 8].map(function(n) {
+      [0, 1, 4, 5, 6, 7].map(function(n) {
         var solutionBoard = new Board(findNQueensSolution(n));
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
           return memo + _.reduce(row, function(memo, col) {
